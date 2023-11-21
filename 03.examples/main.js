6f15b6603f59e127e62c15b6f7761f8c2aba4530
SungminPark <byulparan@gmail.com>
2023-11-22 00:59:29 +0900

    make ambient in 03.examples

--- 03.examples/main.js
+++ 03.examples/main.js
@@ -1,11 +1,15 @@
-/* eslint no-undef: "off", no-unused-vars: "off" */
 
+let initialized = false;
+let audioCtx = new AudioContext();
+let oscils = [];
 
 let pg;
 
 let size = 300;
 let positions = [];
 let rotations = [];
+let lastTime = 0;
+
 function setup() {
   pixelDensity(1.0);
   let canvas = createCanvas(windowWidth , windowHeight);
@@ -18,10 +22,21 @@ function setup() {
     positions.push({x: random(-8,8), y: random(-8,8), z: random(-8,8)});
     rotations.push(random(0, TAU));
   }
+
+  lastTime = audioCtx.currentTime;
 }
 
 function draw() {
-  let time = millis() * 0.001;
+  let time = 0.0
+  if(initialized) { 
+    time = millis() * 0.001; 
+    let curTime = audioCtx.currentTime;
+    if(curTime - lastTime > 4.0) {
+      oscils[2].frequency.value = noteToFreq(choose([64,65,67]));
+      oscils[4].frequency.value = noteToFreq(choose([72,74,79,82]));
+      lastTime = curTime;
+    }
+  } 
   background(0);
   pg.clear();
   pg.perspective(45.0, width/height, .01, 100.0);
@@ -40,7 +55,61 @@ function draw() {
   }
 
   image(pg, 0, 0, pg.width, pg.height);
+}
 
+function mousePressed() {
+  if(!initialized) {
+    initialized = true;
+    audioCtx.resume();
+    sound();
+    sfx();
+  }
 }
 
 
+function sound() {
+  let now = audioCtx.currentTime;
+ 
+  let notes = [48, 60, 64, 70, 74];
+  let gain = audioCtx.createGain();
+  gain.gain.value = 0.0;
+  gain.gain.linearRampToValueAtTime(0.04, now + 2.0);
+  for(let note of notes) {
+    let osc = audioCtx.createOscillator();
+    osc.frequency.value = noteToFreq(note);
+    osc.connect(gain);
+    oscils.push(osc);
+  }
+  gain.connect(audioCtx.destination);
+  for(let osc of oscils) {
+    osc.start(now);
+  }
+}
+
+function sfx() {
+  fetch('/resources/sfx.mp3')
+    .then(data => data.arrayBuffer())
+    .then(arrayBuffer => audioCtx.decodeAudioData(arrayBuffer))
+    .then(decodedAudio => {
+      let now = audioCtx.currentTime;
+      let playSound = audioCtx.createBufferSource();
+      let gain = audioCtx.createGain();
+      gain.gain.value = 0.0;
+      gain.gain.linearRampToValueAtTime(0.6, now + 2)
+      playSound.buffer = decodedAudio;
+      playSound.loop = true;
+      playSound.connect(gain);
+      gain.connect(audioCtx.destination);
+      playSound.start(now);
+    })
+}
+
+
+function noteToFreq(note) {
+  let a = 440; //frequency of A (coomon value is 440Hz)
+  return (a / 32) * (2 ** ((note - 9) / 12));
+}
+
+function choose(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
